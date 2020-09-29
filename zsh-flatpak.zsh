3d0a686828e7e38dceb9932bd68e008976f2b684
Robb Randall <robb.randall@gmail.com>
2020-09-29 12:54:59 -0500

    initial commit

---
+++ zsh-flatpak.zsh
@@ -0,0 +1,18 @@
+
+# Displaying installed flatpaks
+alias flatpaks='flatpak list'
+alias fp-ls='flatpaks'
+
+# Update flatpaks
+alias fp-up='flatpak update'
+
+# Search flatpaks
+alias fp-se='flatpak search'
+alias fp-se-u='flatpak search --user'
+
+# Install flatpaks
+alias fp-in='flatpak install'
+alias fp-in-u='flatpak isntall --user'
+
+# Turn installed applications into functions (used for dmenu)
+# TODO
