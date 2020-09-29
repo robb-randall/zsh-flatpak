afdda6c28b591e71ca3879f4a273b1bcf08e8029
Robb Randall <robb.randall@gmail.com>
2020-09-29 13:24:04 -0500

    removed dash before u in user commands

--- zsh-flatpak.zsh
+++ zsh-flatpak.zsh
@@ -4,19 +4,19 @@ alias flatpaks='flatpak list'
 
 # Install flatpaks
 alias fp-in='flatpak install'
-alias fp-in-u='flatpak isntall --user'
+alias fp-inu='flatpak isntall --user'
 
 # List installed flatpaks
 alias fp-ls='flatpak list'
-alias fp-ls-u='flatpak list --user'
+alias fp-lsu='flatpak list --user'
 
 # Search flatpaks
 alias fp-se='flatpak search'
-alias fp-se-u='flatpak search --user'
+alias fp-seu='flatpak search --user'
 
 # Update flatpaks
 alias fp-up='flatpak update'
-alias fp-up-u='flatpak update --user'
+alias fp-upu='flatpak update --user'
 
 # Turn installed applications into functions (used for dmenu)
 # TODO
