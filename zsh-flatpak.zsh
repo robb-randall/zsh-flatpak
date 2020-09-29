47df610e60bee6360f3efe1e49494c2ec8eb6492
Robb Randall <robb.randall@gmail.com>
2020-09-29 13:06:00 -0500

    fixed alias order

--- zsh-flatpak.zsh
+++ zsh-flatpak.zsh
@@ -1,18 +1,21 @@
 
-# Displaying installed flatpaks
+# List installed flatpaks
 alias flatpaks='flatpak list'
-alias fp-ls='flatpaks'
 
-# Update flatpaks
-alias fp-up='flatpak update'
+# Install flatpaks
+alias fp-in='flatpak install'
+alias fp-in-u='flatpak isntall --user'
+
+# List installed flatpaks
+alias fp-ls='flatpak list'
+alias fp-ls-u='flatpak list --user'
 
 # Search flatpaks
 alias fp-se='flatpak search'
 alias fp-se-u='flatpak search --user'
 
-# Install flatpaks
-alias fp-in='flatpak install'
-alias fp-in-u='flatpak isntall --user'
+# Update flatpaks
+alias fp-up='flatpak update'
 
 # Turn installed applications into functions (used for dmenu)
 # TODO
