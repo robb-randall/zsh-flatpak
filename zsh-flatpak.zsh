5b3f59a493a8f30868ffee2ce4796fc033d19e9b
Robb Randall <robb.randall@gmail.com>
2020-09-30 09:21:48 -0500

    added alias

--- zsh-flatpak.zsh
+++ zsh-flatpak.zsh
@@ -1,6 +1,10 @@
 # List installed flatpaks
 alias flatpaks='flatpak list'
 
+# Show flatpak alias
+alias fp-a='alias | grep "flatpak run"'
+alias fp-alias=fp-a
+
 # Install flatpaks
 alias fp-in='flatpak install'
 alias fp-inu='flatpak install --user'
@@ -17,5 +21,20 @@ alias fp-seu='flatpak search --user'
 alias fp-up='flatpak update'
 alias fp-upu='flatpak update --user'
 
-# Turn installed applications into functions (used for dmenu)
-# TODO
+# Adds a single flatpak alias given the application name
+flatpak_add_alias () {
+	# Using Application name, extract the name and lowercase it
+	local name=$(echo ${1:l} | grep -Po '[^\.]+$')
+
+	# Create the zsh function to launch the flatpak app
+	#$name () { flatpak run $1 }
+
+	# Alias the function
+	alias $name="flatpak run $1"
+	
+}
+
+# Adds an alias for every install flatpak application
+foreach app in $(flatpak list --columns=application --app) ; do
+    flatpak_add_alias $app
+; done
